740a1efb0db0e03ba7a47e2b9a54196a35f802d5
Nick <n.m.phelan@gmail.com>
2023-07-28 18:59:27 +0100

    Working basic calculator JS complete

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,32 @@
 import "./style.scss";
 
 const display = document.querySelector<HTMLInputElement>(".display");
-const output = document.querySelector<HTMLInputElement>(".output");
+const outputDisplay = document.querySelector<HTMLInputElement>(".output");
+const operatorDisplay = document.querySelector<HTMLButtonElement>(".operator");
 const allClear = document.querySelector("#clear");
 const backspace = document.querySelector("#delete");
-const multiply = document.querySelector("#multiply");
-const subtract = document.querySelector("#subtract");
-const divide = document.querySelector("#divide");
-const addition = document.querySelector("#add");
-const percentage = document.querySelector("#percentage");
-const brackets = document.querySelector("#brackets");
-const keypadArr = document.querySelectorAll(".keypad__value");
+// const percentage = document.querySelector("#percentage");
+// const brackets = document.querySelector("#brackets");
+const keypadValueArr = document.querySelectorAll(".keypad__value");
+const keypadOperatorArr =
+  document.querySelectorAll<HTMLButtonElement>(".keypad__operator");
+const equals = document.querySelector<HTMLButtonElement>(".result__operator");
 
 if (
   !display ||
-  !output ||
+  !outputDisplay ||
+  !operatorDisplay ||
   !allClear ||
   !backspace ||
-  !multiply ||
-  !subtract ||
-  !divide ||
-  !addition ||
-  !keypadArr ||
-  !percentage ||
-  !brackets
+  !equals ||
+  !keypadValueArr ||
+  !keypadOperatorArr
+  //   !percentage ||
 ) {
   throw new Error("Variable null error");
 }
+display.value = "0";
+let resultDisplayed = false;
 
 // Searches for nums + .
 const reSearchError = /[^0-9.]/g;
@@ -34,12 +34,12 @@ const reSearchError = /[^0-9.]/g;
 const reSearchOperators = /[-\*\+\/\(\)]/;
 // Checking for double arithmetic inputs
 // take an input, check new input against previous input - if input is non character - don't allow?
-const reDoubleSearch = /[-\*\+\/\(](?=[-\*\+\/\(\)?])/;
+const reDoubleSearch = /[-\*\+\/\(](?=[-\*\+\/\(\)])/;
 
 // Clear the display
 const clearDisplay = () => {
   display.value = "0";
-  output.value = "";
+  outputDisplay.value = "";
 };
 allClear.addEventListener("click", clearDisplay);
 
@@ -52,38 +52,92 @@ backspace.addEventListener("click", deleteLastInput);
 // Update display with keypad values
 const updateDisplay = (event: Event) => {
   const buttonVal = event.target as HTMLButtonElement;
+  if (resultDisplayed) {
+    display.value = "";
+    resultDisplayed = false;
+  }
+  if (display.value == "0") {
+    display.value = "";
+  }
   display.value += buttonVal.innerHTML;
 };
-// Check for repeat operator inputs
+// Check for repeat operator inputs ========= Necessary later w/ scientific
 const handleRepeatInputs = () => {
   if (reDoubleSearch.test(display.value)) {
     display.value = display.value.slice(0, -1);
   }
 };
 
-keypadArr.forEach((button) => {
-  button.addEventListener("click", handleRepeatInputs);
-  button.addEventListener("click", updateDisplay);
-});
-
-// Convert to a percentage when percentage button is clicked
-const returnPercentage = () => {
-  // Will return as boolean if RegEx search matches
-  if (reSearchError.test(display.value)) {
-    output.value = "ERROR";
-    // Acts like delay - wipe input display
-    setTimeout(function () {
-      clearDisplay();
-    }, 2000);
+const handleOperator = (event: Event) => {
+  if (resultDisplayed) {
+    display.value = "";
+    resultDisplayed = false;
+  }
+  if (display.value == "") {
     return;
   }
-  const numToConvert = Number(display.value);
-  display.value = String(numToConvert / 100);
+  const target = event.target as HTMLButtonElement;
+  operatorDisplay.value = target.innerHTML;
+  keypadOperatorArr.forEach((button) => {
+    button.disabled = true;
+  });
+  outputDisplay.value = display.value;
+  display.value = "";
 };
-percentage.addEventListener("click", returnPercentage);
+
+const evaluateEquation = () => {
+  const firstNum = parseFloat(outputDisplay.value);
+  const secondNum = parseFloat(display.value);
+  let result;
+  switch (operatorDisplay.value) {
+    case "+":
+      result = firstNum + secondNum;
+      break;
+    case "-":
+      result = firstNum - secondNum;
+      break;
+    case "/":
+      result = firstNum / secondNum;
+      break;
+    case "*":
+      result = firstNum * secondNum;
+      break;
+  }
+  display.value = String(result);
+  outputDisplay.value = "";
+  operatorDisplay.value = "";
+  resultDisplayed = true;
+  keypadOperatorArr.forEach((button) => {
+    button.disabled = false;
+  });
+};
+
+keypadValueArr.forEach((button) => {
+  button.addEventListener("click", updateDisplay);
+});
+keypadOperatorArr.forEach((button) => {
+  button.addEventListener("click", handleOperator);
+});
+equals.addEventListener("click", evaluateEquation);
+
+// // Convert to a percentage when percentage button is clicked
+// const returnPercentage = () => {
+//   // Will return as boolean if RegEx search matches
+//   if (reSearchError.test(display.value)) {
+//     outputDisplay.value = "ERROR";
+//     // Acts like delay - wipe input display
+//     setTimeout(function () {
+//       clearDisplay();
+//     }, 2000);
+//     return;
+//   }
+//   const numToConvert = Number(display.value);
+//   display.value = String(numToConvert / 100);
+// };
+// percentage.addEventListener("click", returnPercentage);
 
 // Add brackets to value if bracket button is clicked
-const surroundBrackets = () => {
-  display.value = `(${display.value})`;
-};
-brackets.addEventListener("click", surroundBrackets);
+// const surroundBrackets = () => {
+//   display.value = `(${display.value})`;
+// };
+// brackets.addEventListener("click", surroundBrackets);
